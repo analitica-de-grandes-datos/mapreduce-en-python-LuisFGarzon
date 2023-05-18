c63d27769af1edc19982ec9fe6586ac141a89e77
LuisFGarzon <lfgarzona@unal.edu.co>
2023-05-18 18:16:27 -0500

    prueba

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        clean   =   line.strip()
+        key, val = clean.split(",")
+        sys.stdout.write("{}\t{}\n".format(key,val))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    lista1 = []
+
+    for line in sys.stdin:
+        key, val = line.split(",")
+        val = int(val)
+
+        lista1.append((key, val))
+
+        lista1.sort(key=lambda i: i[1], reverse=False)
+
+    for elemento in lista1:
+        sys.stdout.write("{},{}\n".format(elemento[0], elemento[1]))
