88d5fe6e71f9fea6eeeddbdb1d172e84d324c101
LuisFGarzon <lfgarzona@unal.edu.co>
2023-05-19 14:04:35 -0500

    act_preg

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        limpiar =   line.strip()
+        letra,fecha,valor = limpiar.split("   ")
+        sys.stdout.write("{},{},{}\n".format(letra,fecha,valor))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        letra,fecha,valor   =   line.split()
+        valor =   int(valor)
+        sys.stdout.write('{}   {}   {}\n'.format(letra,fecha,valor))
